797832acc0e15b2bff709fd589bb3defeba8d354
JulianManera8 <julianmanera@hotmail.com>
2023-11-28 19:37:33 -0300

    termine de pasarle el estilo a todas las figuras

---
+++ landingpage/js/figura.js
@@ -0,0 +1,32 @@
+var imgPrincipal = document.getElementById('fotoPrincipal');
+let otraFoto = document.getElementsByClassName('otraFoto');
+
+
+otraFoto[0].onclick = function () {
+    imgPrincipal.src = otraFoto[0].src;
+}
+otraFoto[1].onclick = function () {
+    imgPrincipal.src = otraFoto[1].src;
+
+}
+otraFoto[2].onclick = function () {
+    imgPrincipal.src = otraFoto[2].src;
+
+}
+otraFoto[3].onclick = function () {
+    imgPrincipal.src = otraFoto[3].src;
+
+}
+otraFoto[4].onclick = function () {
+    imgPrincipal.src = otraFoto[4].src;
+
+}
+otraFoto[5].onclick = function () {
+    imgPrincipal.src = otraFoto[5].src;
+
+}
+
+
+
+
+
